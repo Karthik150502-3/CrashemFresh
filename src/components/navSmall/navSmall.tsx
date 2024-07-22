0c2198c43fb8fb1441cfa7213dc23809232a72b6
Karthik150502 <karthikrdycool@gmail.com>
2024-07-22 16:33:29 +0530

    Updated the YAML file

--- src/components/navSmall/navSmall.tsx
+++ src/components/navSmall/navSmall.tsx
@@ -17,7 +17,7 @@ export default function NavSmall() {
 
             </div>
             <div className="w-4/5 bg-slate-950 h-full flex flex-row justify-end items-center z-10">
-                <Menu size={44} className='pr-2' strokeWidth="1"  onClick={() => setShowMenu(true)} />
+                <Menu size={44} className='pr-2 hover:size-12 transition-all' strokeWidth="1" onClick={() => setShowMenu(true)} />
             </div>
 
             <section className={clsx('bg-slate-950 absolute -right-44 top-0 h-screen w-40 z-10 pt-24 pl-6 flex flex-col items-center justify-start hide', {
